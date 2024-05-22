ca3bd295e31edaa2b28b9459252cbf7d212334d1
AgusCorrales <aguscorrales1997@gmail.com>
2024-05-22 21:11:02 +0200

    EjerciciosCompletos

---
+++ EjercicioExtra/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Character from './components/characters/Character'
+
+
+function App() {
+  return(
+    <div>
+      <Character/>
+    </div>
+  )
+
+
+
+}
+
+export default App
---
+++ EjercicioExtra/src/components/characters/Character.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'  
+
+const Character = () => {
+    const [printCharacter, setPrintCharacter] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        axios.get("https://swapi.dev/api/people")
+        .then(response=>{
+            console.log(response.data.results);
+            setPrintCharacter(response.data.results);
+            setLoading(false)
+        })
+        .catch(error=>{
+            console.error("Error al cargar resultados",error);
+            setLoading(false)
+        })
+        
+    },[])
+  return (
+    <div>
+        <h1>Personajes de Star Wars</h1>
+        {printCharacter.map(character => (
+        <p key={character.name}>{character.name}</p>
+    ))}
+    </div>
+);
+}
+
+export default Character
